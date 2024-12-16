c7978a8df5dad82f17e825c2477d33f862fca657
Danilo Andrade <danilo_as@live.com>
2024-12-16 07:48:24 -0300

    update16122024

--- pages/09_👨‍🌾_Desenvolvido_por.py
+++ pages/09_👨‍🌾_Desenvolvido_por.py
@@ -40,10 +40,10 @@ st.markdown(
 # Ícones com links sutis
 st.markdown(
     """
-    <a href="https://www.instagram.com/daniloas" target="_blank"><i class="fab fa-instagram"></i> Instagram</a><br>
+    <a href="https://www.instagram.com/daniloas.com_" target="_blank"><i class="fab fa-instagram"></i> Instagram</a><br>
     <a href="https://daniloas.com" target="_blank"><i class="fas fa-globe"></i> Website</a><br>
-    <a href="https://www.linkedin.com/in/daniloas/" target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</a><br>
-    <a href="https://github.com/daniloasdot.com" target="_blank"><i class="fab fa-github"></i> GitHub</a><br>
+    <a href="https://www.linkedin.com/in/daniloandradesantos/" target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</a><br>
+    <a href="https://github.com/daniloasdotcom" target="_blank"><i class="fab fa-github"></i> GitHub</a><br>
     <a href="mailto:danilo_as@live.com" target="_blank"><i class="fas fa-envelope"></i> danilo_as@live.com</a><br>
     """,
     unsafe_allow_html=True
